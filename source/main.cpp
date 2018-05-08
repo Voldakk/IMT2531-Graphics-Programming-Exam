01c0384a2fc3adc325af7107e73d2f9c10e86715
Eivind Vold Aunebakk <eivind@aunebakk.no>
2018-05-08 14:29:39 +0200

    std::experimental::filesystem working on linux

--- source/main.cpp
+++ source/main.cpp
@@ -4,12 +4,16 @@
 #include "../TestScenes/UiTest.hpp"
 #include "../EVA/Editor/SceneEditor.hpp"
 
+#include <experimental/filesystem>
+
 /**
  * \brief The entry point of the program
  * \return An exit code indicating how the program exited
  */
 int main()
 {
+    std::cout << std::experimental::filesystem::current_path() << std::endl;
+
     EVA::Application::Init("EVA-Engine");
 
 	EVA::SceneManager::CreateScene<EVA::SceneEditor>("./assets/scenes/cubes.scene");
